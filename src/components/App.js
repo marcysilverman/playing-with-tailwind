26834d35856542b3215ee99a860c400ec2424d0a
Marcy Silverman <marcysilve225@gmail.com>
2019-03-19 12:08:53 -0700

    changed the font-family

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h2 class="font-sans text-pink text-center pt-6 sm: text-base xl:text-5xl"> Floral Bouquets</h2>
+  )
+}
+
+export default App;
